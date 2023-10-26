6c149a10a71bf7633b1a137b94e333b03306fac7
Biruk Yonas <birukyonas331@gmail.com>
2023-10-27 00:55:24 +0300

    validate fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,18 +4,21 @@
 
 
 def validUTF8(data):
+    """validate a given data if its a utf-8 encoded
     """
-    validates a given data set represents a valid utf-8 encoding
-    the data set contains list of integers
-    """
-
-
-    for i in data:
-        if i > 255:
-            return False
-        try:
-            i.encode('utf-8')
-            continue
-        except UnicodeDecodeError:
-            return False
-    return True    
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
