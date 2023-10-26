606aadb8fdb8eafcbb0d102db54e65fad1eb7fd3
Biruk Yonas <birukyonas331@gmail.com>
2023-10-27 01:08:31 +0300

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""validate a given data if its a utf-8 encoded
+"""validate a given data if its a utf-8 encoded 
 """
 
 
-def validUTF8(data):
-    """validate a given data if its a utf-8 encoded
-    """
 
-    num_bytes = 0
-    for byte in data:
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-    return num_bytes == 0
+def validUTF8(data):
+    """
+    Determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    return not any(
+        byte >> 7 != 0
+        and byte >> 5 != 0b110
+        and byte >> 4 != 0b1110
+        and byte >> 3 != 0b11110
+        for byte in data
+    )
