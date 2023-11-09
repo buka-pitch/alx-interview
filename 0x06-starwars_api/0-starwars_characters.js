f8030d080945ca0fa9ed913798d40c7160c42f58
syco <syco@localhost.localdomain>
2023-11-10 00:54:55 +0300

    starwars api fetcher

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,26 @@
 #!/usr/bin/node
-const request = require('request');
-const BaseUrl = 'https://swapi-api.alx-tools.com/api/';
+const process = require("process");
+const request = require("request");
+const BaseUrl = "https://swapi-api.alx-tools.com/api/";
 
 if (process.argv.length === 3) {
-  getMovieCharachter(parseInt(process.argv[2]));
+  getMovieCharachter(process.argv[2]);
 } else {
-  console.error('Incorrect arguments passed!');
+  console.error("Incorrect arguments passed!");
 }
 
-function getMovieCharachter (id) {
-  request(`${BaseUrl}films`, function (error, response, body) {
+function getMovieCharachter(id) {
+  request(`${BaseUrl}films/${id}`, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       const res = JSON.parse(body);
-      const movie = res.results;
-      movie.forEach((item) => {
-        if (item.episode_id === id) {
-          item.characters.forEach((character) => {
-            request(character, function (error, response, body) {
-              let name = JSON.parse(body);
-              name = name.name;
-              !error && console.log(name);
-              error && console.log(error);
-            });
-          });
-        }
-      });
+      const characters = res.characters;
+
+      for (const i of characters) {
+        request(i, function (error, response, body) {
+          !error && console.log(JSON.parse(body).name);
+          error && console.log(error);
+        });
+      }
     } else {
       console.log(error);
     }
