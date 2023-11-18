2a159ce7acb48da8af3478f8c125be4f2138d21a
Buka <birukyonas331@gmail.com>
2023-11-18 18:41:58 +0000

    matrix 2d rotation

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(Matrix):
+    """
+    Rotate 2D Matrix
+    """
+    N = len(Matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = Matrix[i][j]
+            Matrix[i][j] = Matrix[N - 1 - j][i]
+            Matrix[N - 1 - j][i] = Matrix[N - 1 - i][N - 1 - j]
+            Matrix[N - 1 - i][N - 1 - j] = Matrix[j][N - 1 - i]
+            Matrix[j][N - 1 - i] = temp
